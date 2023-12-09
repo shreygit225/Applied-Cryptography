05ec1095d1679cc631258aef78be4908d0dbebf3
Shrey Gandhi <129980299+shreygit225@users.noreply.github.com>
2023-12-09 15:07:27 +0530

    Update 8_knapsack.py

--- 8_knapsack.py
+++ 8_knapsack.py
@@ -1,86 +1,79 @@
-import random
+# Helper functions
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-def generate_super_increasing_sequence(n):
-    sequence = [random.randint(1, 100)]
-    while len(sequence) < n:
-        next_element = sum(sequence) + random.randint(1, 10)
-        sequence.append(next_element)
-    return sequence
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
 
-def generate_private_key(public_key, q, r):
-    private_key = [(r * element) % q for element in public_key]
-    return private_key
+def is_superincreasing_sequence(seq):
+    for i in range(1, len(seq)):
+        if seq[i] <= sum(seq[:i]):
+            return False
+    return True
 
-def knapsack_encrypt(plaintext, public_key):
-    encrypted_message = sum(public_key[i] for i in range(len(plaintext)) if plaintext[i] == '1')
-    return encrypted_message
+# Encryption functions
+def generate_superincreasing_sequence(private_key, n, m):
+    return [(si * n) % m for si in private_key]
 
-def knapsack_decrypt(ciphertext, private_key, q, r):
-    r_inverse = pow(r, -1, q) 
-    decrypted_message = ''
-    for element in reversed(private_key):
-        if (ciphertext * r_inverse) % q >= element:
-            decrypted_message = '1' + decrypted_message
-            ciphertext -= element
-        else:
-            decrypted_message = '0' + decrypted_message
-    return decrypted_message
+def generate_public_key(private_key, n, m):
+    return generate_superincreasing_sequence(private_key, n, m)
+
+def encrypt(binary_input, public_key):
+    if len(binary_input) % len(public_key) != 0:
+        raise ValueError("Length of binary input must be a multiple of the length of the public key")
+
+    chunk_size = len(public_key)
+    ciphertext = []
+
+    for i in range(0, len(binary_input), chunk_size):
+        chunk = binary_input[i:i + chunk_size]
+        encrypted_value = sum(int(bit) * public_key[j] for j, bit in enumerate(chunk))
+        ciphertext.append(encrypted_value)
+
+        # Show intermediate calculations in output
+        print(f"Encrypting chunk {chunk} using public key {public_key}: {chunk} * {public_key} = {encrypted_value}")
+
+    return ciphertext
+
+def main():
+    # Take user input for private key
+    private_key = [int(bit) for bit in input("Enter the private key as a space-separated superincreasing sequence (e.g., 2 3 7 14): ").split()]
+
+    # Validate superincreasing sequence
+    while not is_superincreasing_sequence(private_key):
+        print("Error: The input is not a superincreasing sequence. Please enter a valid superincreasing sequence.")
+        private_key = [int(bit) for bit in input("Enter the private key as a space-separated superincreasing sequence (e.g., 2 3 7 14): ").split()]
+
+    # Take user input for m (greater than the combined value of the private key)
+    combined_value = sum(private_key)
+    m = int(input(f"Enter the value of m (greater than {combined_value}): "))
+    while m <= combined_value:
+        print("Error: m must be greater than the combined value of the private key.")
+        m = int(input(f"Enter the value of m (greater than {combined_value}): "))
+
+    # Take user input for n (gcd(n, m) should be 1)
+    n = int(input("Enter the value of n (gcd(n, m) should be 1): "))
+    while gcd(n, m) != 1:
+        print("Error: gcd(n, m) must be 1.")
+        n = int(input("Enter the value of n (gcd(n, m) should be 1): "))
+
+    # Generate public key
+    public_key = generate_public_key(private_key, n, m)
+    print("Public Key:", public_key)
+
+    # Take user input for binary message
+    binary_input = input("Enter the binary message to encrypt (e.g., 11010101): ")
+
+    # Encrypt the message
+    ciphertext = encrypt(binary_input, public_key)
+    print("Encrypted Ciphertext:", ciphertext)
 
 if __name__ == "__main__":
-    n = 8 
-    q = 103 
-    r = 3
-
-    public_key = generate_super_increasing_sequence(n)
-    private_key = generate_private_key(public_key, q, r)
-
-    plaintext = input("Enter the text to encrypt: ")
-    ciphertext = knapsack_encrypt(plaintext, public_key)
-    decrypted_message = knapsack_decrypt(ciphertext, private_key, q, r)
-
-    print("Original Message:", plaintext)
-    print("Encrypted Ciphertext:", ciphertext)
-    print("Decrypted Message:", decrypted_message)
-import random
-
-def generate_super_increasing_sequence(n):
-    sequence = [random.randint(1, 100)]
-    while len(sequence) < n:
-        next_element = sum(sequence) + random.randint(1, 10)
-        sequence.append(next_element)
-    return sequence
-
-def generate_private_key(public_key, q, r):
-    private_key = [(r * element) % q for element in public_key]
-    return private_key
-
-def knapsack_encrypt(plaintext, public_key):
-    encrypted_message = sum(public_key[i] for i in range(len(plaintext)) if plaintext[i] == '1')
-    return encrypted_message
-
-def knapsack_decrypt(ciphertext, private_key, q, r):
-    r_inverse = pow(r, -1, q) 
-    decrypted_message = ''
-    for element in reversed(private_key):
-        if (ciphertext * r_inverse) % q >= element:
-            decrypted_message = '1' + decrypted_message
-            ciphertext -= element
-        else:
-            decrypted_message = '0' + decrypted_message
-    return decrypted_message
-
-if __name__ == "__main__":
-    n = 8 
-    q = 103 
-    r = 3
-
-    public_key = generate_super_increasing_sequence(n)
-    private_key = generate_private_key(public_key, q, r)
-
-    plaintext = input("Enter the text to encrypt: ")
-    ciphertext = knapsack_encrypt(plaintext, public_key)
-    decrypted_message = knapsack_decrypt(ciphertext, private_key, q, r)
-
-    print("Original Message:", plaintext)
-    print("Encrypted Ciphertext:", ciphertext)
-    print("Decrypted Message:", decrypted_message)
+    main()
