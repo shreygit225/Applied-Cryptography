21d9cd470fa31ea0d022e95f232f5cdf2c24f572
Shrey Gandhi <129980299+shreygit225@users.noreply.github.com>
2023-12-09 15:05:49 +0530

    Update 6_desalgorithm.py

--- 6_desalgorithm.py
+++ 6_desalgorithm.py
@@ -1,56 +1,68 @@
-
-
-
-ip = [2, 4, 6, 8, 1, 3, 5, 7]
-
-
-expansion_p_box = [1, 2, 3, 4, 4]
-
-
-
-plaintext = input("Enter 8 bit Plain text: ")
-
-
-plaintext = [int(bit) for bit in plaintext]
-
-
-permuted_plaintext = [plaintext[i - 1] for i in ip]
-print("Initial Permuted plain text: ",permuted_plaintext)
-
-
-left_half = permuted_plaintext[:4]
-right_half = permuted_plaintext[4:] 
-print("Left half and Right half: ",left_half,right_half)
-
-
-round_key = "10011"
-round_key = [int(bit) for bit in round_key]
-
-print("Round Key:",round_key)
-
-# round xor right half
-
-expanded_right_half = [right_half[i - 1] for i in expansion_p_box]
-expanded_left_half = [left_half[i - 1] for i in expansion_p_box]
-
-
-print("5 bit left half:", expanded_left_half)
-print("5 bit right half:", expanded_right_half)
-
-key_right = [r^k for r,k in zip(expanded_right_half,round_key)]
-
-print("F(Right,Key): ",key_right)
-
-
-xor_result = [left_bit ^ round_key_bit for left_bit, round_key_bit in zip(expanded_left_half, key_right)]
-print("after key operation with left half:" , xor_result)
-
-straight_p_box = [2, 4, 3, 1]
-new_left_half = [xor_result[i - 1] for i in straight_p_box]
-print("P box operation:", new_left_half)
-
-
-new_plaintext = right_half + new_left_half
-
-
-print("Cipher text (8 bits) after swapping:", ''.join(map(str, new_plaintext)))
+s_box = [
+    [0b0000, 0b0001, 0b0010, 0b0011],
+    [0b0100, 0b0101, 0b0110, 0b0111],
+    [0b1000, 0b1001, 0b1010, 0b1011],
+    [0b1100, 0b1101, 0b1110, 0b1111]
+]
+ 
+# Initial permutation (IP) matrix
+initial_permutation = [2, 6, 3, 1, 4, 8, 5, 7]
+ 
+# Expansion permutation (E) matrix
+expansion_permutation = [4, 1, 2, 3, 2, 3, 4, 1]
+ 
+# P-box permutation matrix
+p_box_permutation = [2, 4, 3, 1]
+ 
+def apply_permutation(block, permutation):
+    result = [block[i - 1] for i in permutation]
+    print("After permutation:", result)
+    return result
+ 
+def split_block(block):
+    result = block[:4], block[4:]
+    print("After splitting:", result)
+    return result
+ 
+def expand(right_half):
+    result = apply_permutation(right_half, expansion_permutation)
+    print("After expansion:", result)
+    return result
+ 
+def substitute(block):
+    row = ((block[0] << 1) + block[3])  # Row is formed from first and fourth bits
+    col = ((block[1] << 1) + block[2])  # Column is formed from the second and third bits
+    result = bin(s_box[row][col])[2:].zfill(4)
+    print("After substitution:", result)
+    return result
+ 
+def permute(block):
+    result = apply_permutation(block, p_box_permutation)
+ 
+    return result
+ 
+def des_round(block, round_key):
+    left_half, right_half = split_block(block)
+    expanded = expand(right_half)
+    xored = [int(expanded[i]) ^ round_key[i] for i in range(8)]
+    substituted = substitute(xored)
+    permuted = permute([int(bit) for bit in substituted])
+    new_right_half = [left_half[i] ^ permuted[i] for i in range(4)]
+    print("After DES round:", right_half + new_right_half)
+    return right_half + new_right_half
+ 
+# Take user input for plaintext and round key
+plaintext_input = input("Enter 8-bit plaintext (e.g., 10100101): ")
+round_key_input = input("Enter 8-bit round key (e.g., 10011010): ")
+ 
+# Convert input strings to lists of integers
+plaintext = [int(bit) for bit in plaintext_input]
+round_key = [int(bit) for bit in round_key_input]
+ 
+# Initial permutation (IP)
+plaintext = apply_permutation(plaintext, initial_permutation)
+ 
+# Perform a single round of DES
+ciphertext = des_round(plaintext, round_key)
+ 
+print("Final Ciphertext:", ciphertext)
